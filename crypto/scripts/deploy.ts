e9fb5448dbe1a1741dcac8164dbbe712a8a0c43e
AKochanski <50527866+AdrianKochanski@users.noreply.github.com>
2023-03-12 11:32:05 +0100

    Crypto module fix

--- crypto/scripts/deploy.ts
+++ crypto/scripts/deploy.ts
@@ -3,6 +3,10 @@ import { BigNumber } from "ethers";
 import { items } from "../assets/items/items.json";
 import { Dappazon } from "../typechain-types";
 
+const tokens = (n: number) => {
+  return ethers.utils.parseUnits(n.toString(10), 'ether')
+}
+
 async function main() {
   const [deployer] = await ethers.getSigners();
   const Dappazon = await ethers.getContractFactory("Dappazon");
@@ -22,14 +26,22 @@ async function main() {
       category: i.category,
       image: i.image,
       name: i.name,
-      cost: BigNumber.from(i.price),
+      cost: BigNumber.from(tokens(Number.parseFloat(i.price))),
       rating: BigNumber.from(i.rating),
       stock: BigNumber.from(i.stock)
     });
+
+    console.log(
+      `Packed items ${i.id}: ${i.name}`
+    );
   });
 
   const transaction = await dappazon.connect(deployer).listItems(arrayItems);
   await transaction.wait();
+
+  console.log(
+    `Listed items count: ${arrayItems.length}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
