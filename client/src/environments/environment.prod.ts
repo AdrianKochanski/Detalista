9504d373842836b8f6cd06d58222254edddbe6c3
AKochanski <50527866+AdrianKochanski@users.noreply.github.com>
2023-05-02 18:00:48 +0200

    production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export default {
   production: true,
-  appUrl: 'https://localhost:5001/',
-  apiUrl:'https://localhost:5001/api/',
+  appUrl: 'https://20.108.188.208:5001/',
+  apiUrl:'https://20.108.188.208:5001/api/',
   crypto: {
     31337: {
-      jsonRPC: "http://127.0.0.1:8545/",
+      jsonRPC: "http://20.108.188.208:8545/",
       dappazon: {
         address: "0xb5ae3e2883934457cb593793f034b401867e1372"
       }
