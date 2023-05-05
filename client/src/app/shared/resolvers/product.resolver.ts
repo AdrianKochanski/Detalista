627bc324e001093d7ad20c749b8f222153e09bc1
AKochanski <50527866+AdrianKochanski@users.noreply.github.com>
2023-05-05 20:17:12 +0200

    Better basket with item quantity in basket

--- client/src/app/shared/resolvers/product.resolver.ts
+++ client/src/app/shared/resolvers/product.resolver.ts
@@ -4,36 +4,61 @@ import {
   RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, map, of, switchMap, take, tap } from 'rxjs';
 import { ShopService } from 'src/app/shop/shop.service';
 import { Product } from '../models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { CryptoShopService } from 'src/app/crypto/crypto-shop.service';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolver implements Resolve<Product> {
+export class ProductResolver implements Resolve<{product: Product, quantityInBasket: number}> {
 
   constructor(
     private shopService: ShopService,
     private bcService: BreadcrumbService,
-    private cryptoService: CryptoShopService
+    private cryptoService: CryptoShopService,
+    private basketService: BasketService
   ){}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<{product: Product, quantityInBasket: number}> {
+    const id = +route.paramMap.get('id');
+    const isCrypto = state.url.includes("crypto");
     this.bcService.set('@productDetails', ' ');
-    return this.getProducts(route, state).pipe(
-      tap(p => this.bcService.set('@productDetails', p.name))
+
+    return this.getProducts(id, isCrypto).pipe(
+      tap(p => this.bcService.set('@productDetails', p.name)),
+      switchMap(p => this.basketService.basket$.pipe(
+        take(1),
+        map(basket => {
+          if(basket && basket.items) {
+            const item = basket.items.find(x => x.id == id);
+
+            if(item && !isCrypto) {
+              return {
+                product: p,
+                quantityInBasket: item.quantity
+              };
+            }
+          }
+
+          return {
+            product: p,
+            quantityInBasket: 0
+          };
+        })
+      ))
     );
   }
 
-  getProducts(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
-    switch(state.url.includes("crypto")) {
+  getProducts(id: number, isCrypto: boolean): Observable<Product> {
+    switch(isCrypto) {
       case true:
-        return this.cryptoService.getProduct(+route.paramMap.get('id'));
+        return this.cryptoService.getProduct(id);
       case false:
-        return this.shopService.getProduct(+route.paramMap.get('id'));
+        return this.shopService.getProduct(id);
     }
   }
 }
--- client/src/app/shared/shop/product-details/product-details.component.ts
+++ client/src/app/shared/shop/product-details/product-details.component.ts
@@ -12,6 +12,7 @@ import { Product } from 'src/app/shared/models/product';
 export class ProductDetailsComponent implements OnInit {
   product: Product;
   quantity = 1;
+  quantityInBasket = 0;
 
   constructor(
     private activateRoute: ActivatedRoute,
@@ -25,33 +26,46 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   loadProduct() {
-    this.activateRoute.data.subscribe((data: {product: Product}) => {
-      this.product = data.product;
+    this.activateRoute.data.subscribe((data: {product: {product: Product, quantityInBasket: number}}) => {
+      this.product = data.product.product;
+      this.quantity = data.product.quantityInBasket ? data.product.quantityInBasket : 1;
+      this.quantityInBasket = data.product.quantityInBasket;
     }, error => {
       console.log(error);
     });
   }
 
   increment() {
-    if(!this.product.isCrypto && this.quantity < this.product.stock) {
+    if(this.product && !this.product.isCrypto && this.quantity < this.product.stock) {
       this.quantity++;
     }
   }
 
   decrement() {
-    if(this.quantity > 1) {
+    if(this.quantity > 0) {
       this.quantity--;
     }
   }
 
-  addItemToBasket() {
-    if(this.product.isCrypto) {
-      this.cryptoService.buyItem(this.product).subscribe(response => {
-        console.log(response);
-      }, e => this.cryptoService.parseMetamaskError(e));
-    }
-    else {
-      this.basketService.addItemToBasket(this.product, this.quantity);
+  updateBasket() {
+    if(this.product) {
+      if(this.product.isCrypto) {
+        this.cryptoService.buyItem(this.product).subscribe(response => {
+          console.log(response);
+        }, e => this.cryptoService.parseMetamaskError(e));
+      }
+      else {
+        if(this.quantity > this.quantityInBasket) {
+          const addAmount = this.quantity - this.quantityInBasket;
+          this.quantityInBasket += addAmount;
+          this.basketService.addItemToBasket(this.product, addAmount);
+        }
+        else {
+          const removeAmount = this.quantityInBasket - this.quantity;
+          this.quantityInBasket -= removeAmount;
+          this.basketService.removeItemFromBasket(this.product.id, removeAmount);
+        }
+      }
     }
   }
 }
