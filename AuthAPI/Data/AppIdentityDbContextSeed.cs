988fc890da57daba7f7bcabcd855dd547acc98a3
AKochanski <50527866+AdrianKochanski@users.noreply.github.com>
2023-10-27 13:23:17 +0200

    User bob and system robot

--- AuthAPI/Data/AppIdentityDbContextSeed.cs
+++ AuthAPI/Data/AppIdentityDbContextSeed.cs
@@ -15,14 +15,15 @@ namespace AuthAPI.Identity
 
         private static async Task SeedUsersAsync(UserManager<AppUser> userManager)
         {
-            if(!userManager.Users.Any()){
+            string TEST_LOGIN = "bob@test.com";
+            if(!userManager.Users.Where(u => u.Email == TEST_LOGIN).Any()){
                 AppUser user = new AppUser{
-                    DisplayName = "Robot",
-                    Email = "robot@robot.com",
-                    UserName = "robot@robot.com",
+                    DisplayName = "Bob",
+                    Email = TEST_LOGIN,
+                    UserName = TEST_LOGIN,
                     Address = new Address{
-                        FirstName = "Robot",
-                        LastName = "Worker",
+                        FirstName = "Bob",
+                        LastName = "Bobby",
                         Street = "10 The Street",
                         City = "New York",
                         State = "NY",
@@ -34,8 +35,29 @@ namespace AuthAPI.Identity
 
                 foreach (Role role in Enum.GetValues(typeof(Role)))
                 {
+                    if(role == Role.SystemRobot) continue;
                     await userManager.AddToRoleAsync(user, role.Name());
                 }
+            }
+
+            string ROBOT_LOGIN = "robot@robot.com";
+            if(!userManager.Users.Where(u => u.Email == ROBOT_LOGIN).Any()){
+                AppUser user = new AppUser{
+                    DisplayName = "Robot",
+                    Email = ROBOT_LOGIN,
+                    UserName = ROBOT_LOGIN,
+                    Address = new Address{
+                        FirstName = "Robot",
+                        LastName = "Worker",
+                        Street = "10 The Street",
+                        City = "New York",
+                        State = "NY",
+                        Zipcode = "90210"
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, Role.SystemRobot.Name());
             }
         }
 
